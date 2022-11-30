94613680d88b82c2e33426be183a98f981a2ee75
tvkalex <tvkalex@yandex.ru>
2022-12-01 00:25:40 +0300

    Add Task3 solution

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным
+(делится ли оно на два без остатка).
+4 -> да
+-3 -> нет
+7 -> нет
+*/
+
+System.Console.WriteLine("Введите число для проверки на четность (ДА) / нечетность (НЕТ): ");
+int num = int.Parse(System.Console.ReadLine()!);
+
+if (num % 2 == 0)
+{
+    System.Console.WriteLine("ДА");
+}
+else
+{
+    System.Console.WriteLine("НЕТ");
+}
+
+//Console.WriteLine((int.Parse(Console.ReadLine()) & 1) == 0? "Yes": "No");
+//& 1 - побитовая операция "И" в двоичной системе, сравниваем число с единицей,
+//проверяем крайний правый бит на 1 (нечетный), 0 (четный).
