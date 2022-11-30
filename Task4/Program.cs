eaa13cad161344da2817df145e1ca1bb08570ef1
tvkalex <tvkalex@yandex.ru>
2022-12-01 01:25:18 +0300

    Add Task4 solution

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -11,5 +11,5 @@ for (int i = 1; i <= N; i++)
     Console.Write(i + " ");
 }
 
-//Решение в одну строчку. Вот к чему нужно стремиться ))
+//Решение в одну строчку. Вот к чему нужно стремиться ☺
 //foreach (int i in Enumerable.Range(1, int.Parse(Console.ReadLine()) >> 1)) Console.WriteLine(i << 1);
