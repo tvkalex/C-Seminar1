a151da81c49393791211a27d453251e4f6d132ae
tvkalex <tvkalex@yandex.ru>
2022-12-01 01:15:29 +0300

    Add Task2 solution

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -6,11 +6,11 @@
 */
 
 System.Console.WriteLine("Введите первое число: ");
-int num1 = int.Parse(System.Console.ReadLine()!);
+int num1 = int.Parse(System.Console.ReadLine()!); //Add ! as null forgiving to avoid mistake "Преобразование литерала, допускающего значение NULL или возможного значения NULL в тип, не допускающий значение NULL." [Ex002_HelloUser]csharp(CS8600)
 System.Console.WriteLine("Введите второе число: ");
-int num2 = int.Parse(System.Console.ReadLine()!);
+int num2 = int.Parse(System.Console.ReadLine()!); //Add ! as null forgiving to avoid mistake "Преобразование литерала, допускающего значение NULL или возможного значения NULL в тип, не допускающий значение NULL." [Ex002_HelloUser]csharp(CS8600)
 System.Console.WriteLine("Введите третье число: ");
-int num3 = int.Parse(System.Console.ReadLine()!);
+int num3 = int.Parse(System.Console.ReadLine()!); //Add ! as null forgiving to avoid mistake "Преобразование литерала, допускающего значение NULL или возможного значения NULL в тип, не допускающий значение NULL." [Ex002_HelloUser]csharp(CS8600)
 
 int max = num1;
 
@@ -18,3 +18,6 @@ if (num2 > max) max = num2;
 if (num3 > max) max = num3;
 
 Console.WriteLine("Максимальное число из {0}, {1}, {2} -> равно {3}", num1, num2, num3, max);
+
+//Решение в одну строчку. Вот к чему нужно стремиться ))
+//Console.WriteLine(Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).Max());
