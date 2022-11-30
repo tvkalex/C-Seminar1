3454856d7694800abd03be5ba680777bd3381ea0
tvkalex <tvkalex@yandex.ru>
2022-12-01 01:14:15 +0300

    Add Task4 solution

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Напишите программу, которая на вход принимает число (N), а на выходе
+показывает все чётные числа от 1 до N.
+*/
+
+System.Console.WriteLine("Введите число: ");
+int N = int.Parse(System.Console.ReadLine()!); //Add ! as null forgiving to avoid mistake "Преобразование литерала, допускающего значение NULL или возможного значения NULL в тип, не допускающий значение NULL." [Ex002_HelloUser]csharp(CS8600)
+for (int i = 1; i <= N; i++)
+{
+    if(i % 2 == 0)
+    Console.Write(i + " ");
+}
+
+//Решение в одну строчку. Вот к чему нужно стремиться ))
+//foreach (int i in Enumerable.Range(1, int.Parse(Console.ReadLine()) >> 1)) Console.WriteLine(i << 1);
