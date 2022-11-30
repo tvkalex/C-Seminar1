1b2c4af64b61ee8d621aca500bda4dd0bbd0ea1d
tvkalex <tvkalex@yandex.ru>
2022-12-01 01:15:12 +0300

    Add Task3 solution

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -7,7 +7,7 @@
 */
 
 System.Console.WriteLine("Введите число для проверки на четность(ДА)/нечетность(НЕТ): ");
-int num = int.Parse(System.Console.ReadLine()!);
+int num = int.Parse(System.Console.ReadLine()!); //Add ! as null forgiving to avoid mistake "Преобразование литерала, допускающего значение NULL или возможного значения NULL в тип, не допускающий значение NULL." [Ex002_HelloUser]csharp(CS8600)
 
 if (num % 2 == 0)
 {
@@ -18,6 +18,7 @@ else
     System.Console.WriteLine("НЕТ");
 }
 
+//Решение в одну строчку. Вот к чему нужно стремиться ))
 //Console.WriteLine((int.Parse(Console.ReadLine()) & 1) == 0? "Yes": "No");
 //& 1 - побитовая операция "И" в двоичной системе, сравниваем число с единицей,
 //проверяем крайний правый бит на 1 (нечетный), 0 (четный).
